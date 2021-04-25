14f4da77ffada076ef2c13cab30ad03fdb33cf1c
Reshma <reshmasingh032@gmail.com>
2021-04-25 23:18:30 +0530

    Updates

---
+++ precache-manifest.5d6a10869882f7991e40fb0b86b0554d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33bfa992d7615fd19dd9ddbd954977ff",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "0290228e49a1ee6038bf",
+    "url": "/covid-tracker/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "0b5449319f7c134d0e71",
+    "url": "/covid-tracker/static/css/main.806f294b.chunk.css"
+  },
+  {
+    "revision": "0290228e49a1ee6038bf",
+    "url": "/covid-tracker/static/js/2.3ecd27d7.chunk.js"
+  },
+  {
+    "revision": "2147958a3c6b33c12a54250c804f4de1",
+    "url": "/covid-tracker/static/js/2.3ecd27d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5449319f7c134d0e71",
+    "url": "/covid-tracker/static/js/main.4ef6ddf6.chunk.js"
+  },
+  {
+    "revision": "5526da79161c5aa71367",
+    "url": "/covid-tracker/static/js/runtime-main.8e99c690.js"
+  }
+]);
